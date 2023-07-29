34c2e7ca4695327d0970a2d9b6630b2b8cdf26d8
Subhash Sagara Singh <sseffort@gmail.com>
2023-07-29 15:59:39 +0530

    using php

---
+++ admin/footer.php
@@ -0,0 +1,13 @@
+<!-- Footer -->
+<div id ="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <span>© Copyright 2023 Vlog Site | Powered by <a href="#">Subhash Sagar Singh</a></span>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- /Footer -->
+</body>
+</html>
